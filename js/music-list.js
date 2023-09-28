b8335962d922f299f14f7f87d47111fd7c44316b
Vishnu Unnikrishnan <84119817+speedyboy6@users.noreply.github.com>
2023-09-28 12:26:41 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "NEON BLADE - MoonDeity",
+    artist: "MoonDeity",
+    img: "neon-blade",
+    src: "neon-blade"
+  },
+  {
+    name: "Sahara - Hensonn",
+    artist: "Hensonn",
+    img: "sahara",
+    src: "sahara"
+  },
+  {
+    name: "After Dark - Mr.Kitty",
+    artist: "Mr.Kitty",
+    img: "neon-blade",
+    src: "after-dark"
+  },
+  {
+    name: "Wake Up - MoonDeity",
+    artist: "MoonDeity",
+    img: "wake-up",
+    src: "wake-up"
+  },
+  {
+    name: "Murder In My Mind - Kordhell",
+    artist: "Kordhell",
+    img: "murder-in-my-mind",
+    src: "murder-in-my-mind"
+  },
+  {
+    name: "Scopin - Kordhell",
+    artist: "Kordhell",
+    img: "scopin",
+    src: "scopin"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
