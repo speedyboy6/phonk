e9ee24c9f9a86533185148946dd1c558ccacf95e
Vishnu Unnikrishnan <84119817+speedyboy6@users.noreply.github.com>
2023-09-30 20:37:04 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,128 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "NEON BLADE - MoonDeity",
+    artist: "MoonDeity",
+    img: "neon - Copy (2)",
+    src: "neon-blade"
+  },
+  {
+    name: "Sahara - Hensonn",
+    artist: "Hensonn",
+    img: "neon - Copy (3)",
+    src: "sahara"
+  },
+  {
+    name: "After Dark - Mr.Kitty",
+    artist: "Mr.Kitty",
+    img: "neon - Copy (4)",
+    src: "after-dark"
+  },
+  {
+    name: "Wake Up - MoonDeity",
+    artist: "MoonDeity",
+    img: "neon - Copy (5)",
+    src: "wake-up"
+  },
+  {
+    name: "Murder In My Mind - Kordhell",
+    artist: "Kordhell",
+    img: "neon - Copy (6)",
+    src: "murder-in-my-mind"
+  },
+  {
+    name: "Scopin - Kordhell",
+    artist: "Kordhell",
+    img: "sneon - Copy (7)",
+    src: "scopin"
+  },
+  {
+    name: "Freestyle - Big Rocky",
+    artist: "Big Rocky",
+    img: "neon - Copy (8)",
+    src: "freestyle"
+  },
+  {
+    name: "Wig Split - Kordhell",
+    artist: "Kordhell",
+    img: "neon - Copy (9)",
+    src: "wigsplit"
+  },
+  {
+    name: "DOWNFALL",
+    artist: "",
+    img: "neon - Copy (10)",
+    src: "downfall"
+  },
+  {
+    name: "METAMORPHOSIS",
+    artist: "INTERWORLD",
+    img: "metamorphosis",
+    src: "metamorphosis"
+  },
+  {
+    name: "RAVE - Dxrk",
+    artist: "Dxrk",
+    img: "rave",
+    src: "rave"
+  },
+  {
+    name: "GigaChad Theme",
+    artist: "GigaChad",
+    img: "gigachad",
+    src: "gigachad"
+  },
+  {
+    name: "WORTH NOTHING",
+    artist: "Denial",
+    img: "worthnothing",
+    src: "worthnothing"
+  },
+  {
+    name: "MUCHUCA MUCHUCA",
+    artist: "MC KRANKEN",
+    img: "muchuca",
+    src: "muchuca"
+  },
+  {
+    name: "Come as you Phonk",
+    artist: "lofi'chield",
+    img: "comeasyouphonk",
+    src: "comeasyouphonk"
+  },
+  {
+    name: "DARKSIDE",
+    artist: "CHMCL SOUP",
+    img: "darkside",
+    src: "darkside"
+  },
+  {
+    name: "ANUBIS",
+    artist: "KUTE & RAIZHELL",
+    img: "anubis",
+    src: "anubis"
+  },
+  {
+    name: "MUDD",
+    artist: "$werve, Grioten & Day$okee",
+    img: "mudd",
+    src: "mudd"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
